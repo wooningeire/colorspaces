b27c43dc1d7abe04e76ef2e6c1d9c79f4ca345b6
​ <>
2022-05-13 02:02:46 -0400

    Add ability to load Pyodide (currently thread-blocking)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {createApp} from "vue";
 import Root from "./components/Root.vue";
 
-// import "./pyodide/index";
-
 createApp(Root).mount("main");
+
+import "./pyodide/index";
--- src/pyodide/index.ts
+++ src/pyodide/index.ts
@@ -1,19 +1,21 @@
 
-// // import "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js";
-// import {loadPyodide} from "./pyodide/pyodide";
+// import "https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js";
+import {loadPyodide} from "./pyodide/pyodide";
 
-// // const pack = await import("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
+// const pack = await import("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
 
-// const pyodide = await loadPyodide();
-// await pyodide.loadPackage("micropip");
+const pyodide = await loadPyodide();
+await pyodide.loadPackage("micropip");
 
-// await pyodide.runPythonAsync(`
-// # workaround presented by https://github.com/pyodide/pyodide/issues/1603#issuecomment-850794345 
-// import sys
-// sys.modules["_multiprocessing"] = object()
+await pyodide.runPythonAsync(`
+# workaround presented by https://github.com/pyodide/pyodide/issues/1603#issuecomment-850794345 
+import sys
+sys.modules["_multiprocessing"] = object()
 
-// import micropip
-// await micropip.install("colour-science")
-// `);
+import micropip
+await micropip.install("colour-science")
+`);
 
-// const colour = pyodide.pyimport("colour");
+export const colour = pyodide.pyimport("colour");
+
+console.info("Pyodide colour_science ready!");
