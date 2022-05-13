cf64d617d4dff219ee9cffc695b9611925a26e0b
​ <>
2022-05-13 02:32:40 -0400

    Offload Pyodide loading to Worker

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import Root from "./components/Root.vue";
 
 createApp(Root).mount("main");
 
-import "./pyodide/index";
+const colourWorker = new Worker(new URL("./pyodide-worker/index.ts", import.meta.url), {type: "module"});
+colourWorker.addEventListener("message", console.log);
--- src/pyodide-worker/load-colour.ts
+++ src/pyodide-worker/load-colour.ts
@@ -4,11 +4,11 @@ import {loadPyodide} from "./pyodide/pyodide";
 
 // const pack = await import("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
 
-const pyodide = await loadPyodide();
+export const pyodide = await loadPyodide();
 await pyodide.loadPackage("micropip");
 
+// object() workaround presented by https://github.com/pyodide/pyodide/issues/1603#issuecomment-850794345 
 await pyodide.runPythonAsync(`
-# workaround presented by https://github.com/pyodide/pyodide/issues/1603#issuecomment-850794345 
 import sys
 sys.modules["_multiprocessing"] = object()
 
@@ -18,4 +18,4 @@ await micropip.install("colour-science")
 
 export const colour = pyodide.pyimport("colour");
 
-console.info("Pyodide colour_science ready!");
+console.info("Pyodide colour-science ready!");
