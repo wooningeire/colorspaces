783f2e7657651403a78704d8b30d702855a2d908
​ <>
2022-05-14 15:08:32 -0400

    Port chromatic adaptation algorithm

--- src/models/colormanagement.ts
+++ src/models/colormanagement.ts
@@ -41,8 +41,30 @@ export const xyz2degToLinear = (xyz: Color, /* illuminantXyz: Color */) => {
 };
 
 // https://www.mathworks.com/help/images/ref/whitepoint.html
-const illuminantXyzs = {
+const illuminantsXyz = {
 	icc: [31595, 32768, 27030].map(comp => comp / 32768),
-	d65: [0.9504, 1, 1.0888],
+	d50: [0.9642956764295677, 1, 0.8251046025104602] as Color,
+	d65: [0.9504559270516716, 1, 1.0890577507598784] as Color,//[0.9504, 1, 1.0888] as Color,
 	e: [1, 1, 1],
+};
+
+const chromaticAdaptationTransforms = {
+	"Bradford": [
+		[ 0.8951,  0.2664, -0.1614],
+		[-0.7502,  1.7135,  0.0367],
+		[ 0.0389, -0.0685,  1.0296],
+	],
+};
+
+/**
+ * Adapted from https://github.com/colour-science/colour/blob/develop/colour/adaptation/vonkries.py#L44
+ */
+const chromaticAdaptationMat = (testWhiteXyz: Color, refWhiteXyz: Color, adaptationMatrix: number[][]) => {
+	const newTestWhiteXyz = math.multiply(adaptationMatrix, testWhiteXyz);
+	const newRefWhiteXyz = math.multiply(adaptationMatrix, refWhiteXyz);
+
+	const scalarMatrix = math.diag(math.dotDivide(newRefWhiteXyz, newTestWhiteXyz) as math.MathCollection);
+
+	const adaptationShifterMatrix = math.multiply(math.inv(adaptationMatrix), scalarMatrix);
+	return math.multiply(adaptationShifterMatrix, adaptationMatrix);
 };
